c8aa14b9f5a39592f470e6bef95bf1e99df8b753
Choon Keat <behchoonkeat@hotmail.com>
2017-06-12 00:08:37 +0800

    improve css

--- apps/modules/menu2/Views/menu2.php
+++ apps/modules/menu2/Views/menu2.php
@@ -1,8 +1,8 @@
 <div>
 	<header class="page-header">
 		
-		<section id="top_nav" class="container">
-			<div class="pull-left">
+		<section id="top_nav" class="container max-width">
+			<div class="pull-left col-md-offset-5 col-sm-offset-5 col-xs-offset-3 col-lg-offset-0">
 				<ul class="list-inline">
 					<li><a href="#"><i class="fa fa-envelope"></i><span class="">  hello@ziumlight.com</span></a></li>
 				</ul>
@@ -17,19 +17,69 @@
 			</div>
 		</section>
 		
-		<section id="menu_bar" class="container">
+		<section id="menu_bar" class="container max-width">
 			<div class="pull-left">
-				<div class="col-sm-4 col-lg-5  col-md-4 col-xs-5 no-left-padding">
+				<div class="col-sm-5 col-lg-3 col-md-6 col-xs-5 no-left-padding">
 					<div id="logo">
 						<a href="#"><img alt="Ziumlight" class="img-responsive" title="Ziumlight" src="<?php echo base_url('img/catalog/logo.png');?>"></a>
 					</div>
 				</div>
-				<div class="col-lg-4 col-md-4 hidden-xs hidden-sm">
-					<ul class="list-inline">
+				<div class="col-lg-4 hidden-xs hidden-sm hidden-md collapse navbar-collapse navbar-ex1-collapse">
+					<ul class="list-inline nav navbar-nav hidden-md hidden-sm hidden-xs">
 						<li><a href="#">New</a></li>
+						<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Furniture</a>
+							<div class="dropdown-menu">
+            					<div class="dropdown-inner">
+            						<ul class="list-unstyled">
+            							<li><a href="#">Bed</a></li>
+            							<li><a href="#">Sofa</a></li>
+            							<li><a href="#">Table</a></li>
+            						</ul>
+            						<ul class="list-unstyled">
+            							<li><a href="#">Apple</a></li>
+            							<li><a href="#">Pear</a></li>
+            							<li><a href="#">Orange</a></li>
+            						</ul>
+            					</div>
+            				</div>
+						</li>
+						<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Furnishings</a>
+							<div class="dropdown-menu">
+            					<div class="dropdown-inner">
+            						<ul class="list-unstyled">
+            							<li><a href="#">Bed</a></li>
+            							<li><a href="#">Sofa</a></li>
+            							<li><a href="#">Table</a></li>
+            						</ul>
+            						<ul class="list-unstyled">
+            							<li><a href="#">Apple</a></li>
+            							<li><a href="#">Pear</a></li>
+            							<li><a href="#">Orange</a></li>
+            						</ul>
+            					</div>
+            				</div>
+						</li>
+						<li><a href="#">Sale</a></li>
 					</ul>
 				</div>
-				
+				<div class="col-lg-4 hidden-md hidden-sm hidden-xs">
+					<div id="search" class="input-group">
+						<input class="form-control input-md col-lg-12" name="search" value="" placeholder="Search" type="text"/>
+						<span class="input-group-btn">
+							<button class="btn btn-info btn-md" type="button">
+								<i class="fa fa-search"></i>
+							</button>
+						</span>
+					</div>
+				</div>
+			</div>
+			
+			<div class="pull-right">
+				<ul class="list-inline">
+					<li class="hidden-lg"><a href="#"><i class="fa fa-search big-icon"></i></a></li>
+					<li><a href="#"><i class="fa fa-user big-icon"></i></a></li>
+					<li><a href="#"><i class="fa fa-shopping-cart big-icon"></i></a></li>
+				</ul>
 			</div>
 		</section>
 	</header>
