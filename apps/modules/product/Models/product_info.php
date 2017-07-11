040d067e46aec9f90b29ba85adc037c210db4938
Choon Keat <behchoonkeat@hotmail.com>
2017-07-11 23:40:36 +0800

    option for product

--- apps/modules/product/Models/product_info.php
+++ apps/modules/product/Models/product_info.php
@@ -7,7 +7,27 @@ class Prd_Info{
 	public $additionalInfo;
 	public $discountPercent;
 	public $discountPrice;
+	public $optionsList;
 }
+
+class Prd_Options{
+	public $name;
+	public $options = array();
+	function __construct($optionName){
+		$this->name = $optionName;
+	}
+}
+
+class Prd_SubOption{
+	public $name;
+	public $src;
+	
+	function __construct($subOptionName, $srcPath){
+		$this->name = $subOptionName;
+		$this->src = $srcPath;
+	}
+}
+
 
 class product_info extends CI_Model{
 	function __construct(){
@@ -21,9 +41,21 @@ class product_info extends CI_Model{
 		$prd->discountPercent = '35%';
 		$prd->discountPrice = '45.00';
 		$prd->price = '70.00';
+		$prd->optionsList = $this->getProductOptionsList();
 		$prd->additionalInfo = array();
 		array_push($prd->additionalInfo, 'Free Delivery');
 		return $prd;
+	}
+	
+	public function getProductOptionsList(){
+		$options = array();
+		$src = 'img/catalog/shoe/option.png';
+		$options1 = new Prd_Options('Colour');
+		array_push($options1->options, new Prd_SubOption('Red', $src), new Prd_SubOption('Green', $src));
+		$options2 = new Prd_Options('Model');
+		array_push($options2->options, new Prd_SubOption('Square', $src), new Prd_SubOption('Circle', $src));
+		array_push($options, $options1, $options2);
+		return $options;
 	}
 	
 }
--- apps/modules/product/Views/product_info.php
+++ apps/modules/product/Views/product_info.php
@@ -1,4 +1,4 @@
-<div class="col-md-5 col-sm-12 col-12">
+<div class="col-md-5 col-sm-12 col-12 center">
 	<div class="col-12">
 		<h2><?php echo $product->name;?></h2>
 	</div>
@@ -10,8 +10,25 @@
 		<h3 class="final_price">$<?php echo $product->discountPrice;?></h3>
 		<span class="display-block">Discount <?php echo $product->discountPercent;?></span>
 		<span class="display-block">Usual Price $<?php echo $product->price;?></span>
-		
-		
 	</div>
-	
+	<div style="padding-top: 10px;">
+	<?php if (isset($product->optionsList)){
+			foreach($product->optionsList as $options){?>
+			<div class="col-12">
+				<span class="display-block productOptionName"><?php echo $options->name; ?></span>
+				<?php if (isset($options) && isset($options->options)){?>
+					<ul class="list-inline">
+						<?php foreach ($options->options as $option){ ?>
+							<li class="list-inline-item"><a href="#">
+								<div class="<?php if (true){ echo 'selectedProductOption';}?>">
+									<img width="53" height="53" alt="<?php echo $option->name; ?>" src="<?php echo $option->src; ?>">
+								</div>
+								</a>
+							</li>
+						<?php }?>
+					</ul>
+				<?php }?>
+			</div>
+	<?php }}?>
+	</div>
 </div>
